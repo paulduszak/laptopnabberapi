9c8505521bdc6a02aa2b278d41be741985c7a5f5
Paul Duszak <duszak.paul@gmail.com>
2018-03-12 02:29:07 -0500

    Began stubbing out BestBuy API router

---
+++ api/bestbuy.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+var bby = require('bestbuy')('***REMOVED***');
+
+router.get('/', function(req, res) {
+    bby.products('(categoryPath.id=abcat0502000)', {show: 'name', format: 'json', pageSize: 1}, function(err, data) {
+        if (err) console.warn(err);
+        else if (data.total === 0) res.send('No products found');
+        else res.json(data);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const bestbuyApi = require('./api/bestbuy');
+
+app.use('/api/bestbuy', bestbuyApi);
+
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello from express.' });
 });
