32b8e9b01b9a71ee1f9edc5f7af689f04cb89fa2
Paul Duszak <duszak.paul@gmail.com>
2018-07-05 19:55:53 -0500

    Updated archiver to store data in DB and exit once done

--- archiver/index.js
+++ archiver/index.js
@@ -17,73 +17,58 @@ mongoose.connection.on('error', (err) => {
 
 const walkBBAPI = (cursor = '*') =>
   Parsers.bestbuy(cursor)
-    .then((data) => {
+    .then(data => {
       if (data.products.length) {
         return walkBBAPI(data.nextCursorMark)
           .then(nextData => data.products.concat(nextData)).catch(err => console.error(err));
       } else {
         return data.products;
       }
-
     }).catch(err => console.error(err));
 
-      // for (key in data.products) {
-      //   //console.log(data)
-      //   let laptop = new Laptop({
-      //     manufacturer: data.products[key].manufacturer,
-      //     name: data.products[key].name,
-      //     modelNumber: data.products[key].modelNumber,
-      //     sku: data.products[key].sku,
-      //     details: data.products[key].details,
-      //     color: data.products[key].color,
-      //     thumbnailImage: data.products[key].thumbnailImage,
-      //     image: data.products[key].image
-      //   });
-        
-      //   Laptop
-      //     .update(
-      //       {sku: data.products[key].sku},
-      //       {$setOnInsert: laptop},
-      //       {upsert: true})
-      //     .then((doc) => {})
-      //     .catch((err) => {
-      //       return console.err(err);
-      //     })
+const updateBBDB = (products) => {
+  let queries = [];
+  for (key in products) {
     
-      //   LaptopPricing
-      //     .findOneAndUpdate(
-      //       { sku: data.products[key].sku },
-      //       {
-      //         $set: {
-      //           sku: data.products[key].sku,
-      //           BB_regularPriceDayAvg: data.products[key].regularPrice,
-      //           BB_salePriceDayAvg: data.products[key].salePrice
-      //         },
-      //         $push: {
-      //           BB_regularPriceHours: data.products[key].regularPrice,
-      //           BB_salePriceHours: data.products[key].salePrice
-      //         }
-      //       }, 
-      //       {
-      //         upsert: true,
-      //         setDefaultsOnInsert: true
-      //       })
-      //     .then((doc) => {})
-      //     .catch((err) => {
-      //       return console.error(err);
-      //     })
-      // }
+    let laptop = new Laptop({
+      manufacturer: products[key].manufacturer,
+      name: products[key].name,
+      modelNumber: products[key].modelNumber,
+      sku: products[key].sku,
+      details: products[key].details,
+      color: products[key].color,
+      thumbnailImage: products[key].thumbnailImage,
+      image: products[key].image
+    });
+    
+    queries.push(Laptop
+      .update(
+        {sku: products[key].sku},
+        {$setOnInsert: laptop},
+        {upsert: true}).exec());
 
-      // if(data.products.length != 0){
-      //   console.log(data.products.length)
-      //   return walkBBAPI(data.nextCursorMark, 20);
-      // }
-
-    //});
-
+    queries.push(LaptopPricing
+      .findOneAndUpdate(
+        { sku: products[key].sku },
+        {
+          $set: {
+            sku: products[key].sku,
+            BB_regularPriceDayAvg: products[key].regularPrice,
+            BB_salePriceDayAvg: products[key].salePrice
+          },
+          $push: {
+            BB_regularPriceHours: products[key].regularPrice,
+            BB_salePriceHours: products[key].salePrice
+          }
+        }, 
+        {
+          upsert: true,
+          setDefaultsOnInsert: true
+        }).exec());
+  }
+  return queries;
+}
 
 walkBBAPI()
-  .then(result => {
-    console.log(result.length)
-    process.exit()
-  })
+.then(result => Promise.all(updateBBDB(result)))
+.then((() => {process.exit()}))
