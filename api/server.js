8d281e80da0507e1468b1fa8654a5449da3a12a2
Paul Duszak <duszak.paul@gmail.com>
2018-11-06 02:14:54 -0600

    Added react-scripts dependency; Moved server.js to API folder as it runs the API server

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const bestbuyApi = require('./api/bestbuy');
+const bestbuyApi = require('./bestbuy');
 
 app.use('/api/bestbuy', bestbuyApi);
 
